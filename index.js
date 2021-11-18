db433c09ab873513027047b7c0c7d09293a9b0c5
Jay Lee <jay.j.lee.1@gmail.com>
2021-11-17 21:38:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCat = [...cats, name];
+    return newCat
+    
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats
+}
+
+function removeLastCat() {
+    const sliceCat = cats.slice(0, -1);
+    return sliceCat
+}
+
+function removeFirstCat() {
+    const removeFirst = cats.slice(1)
+    return removeFirst
+}
